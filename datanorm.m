f407c89294ac7e80104cf4806084cb4afe34ee24
aseshasa <aseshasa@eng.ucsd.edu>
2017-06-11 22:24:35 -0700

    Delete datanorm.m

--- datanorm.m
+++
@@ -1,7 +0,0 @@
-function [y, Hs] = datanorm(x)
-h = size(x,1);
-mu = mean(x, 2);
-sigma2 = sum(var(x, 0, 2));
-s = sqrt((h-1)/sigma2);
-Hs = [s*[eye(h-1), -s*mu(1:h-1)]; [zeros(1,h-1) 1]];
-y = Hs*x;
